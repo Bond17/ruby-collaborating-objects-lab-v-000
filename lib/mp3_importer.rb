b95f41e716ac0bab80c712306a4de137621a69f8
Robert Watson <spartan83@gmail.com>
2017-12-31 23:09:28 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,11 +4,11 @@ class MP3Importer
   def initialize(path)
     @path=path
   end
-  def self.files#files
+  def files#files
 
   end
 
-  def self.import(list_of_filenames)#import
+  def import(list_of_filenames)#import
 
   end
 end
