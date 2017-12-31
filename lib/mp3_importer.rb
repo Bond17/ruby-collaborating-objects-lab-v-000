a30baa0775cd9fedf3cbf473ff9abc6c144a8aed
Robert Watson <spartan83@gmail.com>
2017-12-31 23:26:29 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,6 +9,6 @@ class MP3Importer
   end
 
   def import#import
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename)}
+    @path.each{ |filename| Song.new_by_filename(filename)}
   end
 end
